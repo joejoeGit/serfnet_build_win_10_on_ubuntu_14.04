2789101e8473d38f4453022eb55df79887669ac6
J B <37249474+seekthex@users.noreply.github.com>
2018-03-30 18:28:19 -0400

    Update install-chc.sh

--- install-chc.sh
+++ install-chc.sh
@@ -15,74 +15,88 @@ message() {
 }
 
 
-createfirewall() {
 
+createfirewall() {
 	sudo ufw allow 21994
-	
 	sudo ufw allow 21995
-	
 	sudo ufw default deny incoming
-	
 	sudo ufw default allow outgoing
-	
 	sudo ufw enable 
-	
 	#sudo reboot
 }
 
 
+
 createswap() { #TODO: add error detection
-
 	message "Creating 2GB permament swap file...this may take a few minutes..."
-	
 	sudo dd if=/dev/zero of=/swapfile bs=1M count=2000	
-
 	sudo mkswap /swapfile
-
 	sudo chown root:root /swapfile
-
 	sudo chmod 0600 /swapfile
-
 	sudo swapon /swapfile
-
 	sudo chmod 0600 /swapfile
-
 	sudo chown root:root /swapfile
-
 	sudo echo "/swapfile none swap sw 0 0" >> /etc/fstab
 }
 
 
 
-
-
-
-
-
-
 prepdependencies() { #TODO: add error detection
 
-
 	message "Installing dependencies..."	
+	
 	# General
 	sudo apt update
+	# Extras
 	sudo apt-get git
-	sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 
-	# Boost C macros - Bitcoin core trying to remove this
-	sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
-	# Berkeley Db - Some duplication - script is used later
+		
+	#Linux Distrobution specific intallation (Unbuntu Debinan)
+	
+	sudo apt-get install build-essential 
+	sudo apt-get install libtool 
+	sudo apt-get install autotools-dev
+	sudo apt-get install automake pkg-config 
+	sudo apt-get installlibssl-dev
+	sudo apt-get install libevent-dev 
+	sudo apt-get installbsdmainutils 
+	sudo apt-get install python3 
+	
+}
+	
+
+
+makeboost{
+	#Linux Distrobution specific (Ubuntu 14.04+ Debinan 7+)
+	sudo apt-get install 
+	sudo apt-get libboost-system-dev 
+	sudo apt-get libboost-filesystem-dev 
+	sudo apt-get libboost-chrono-dev
+	sudo apt-get libboost-program-options-dev
+	sudo apt-get libboost-test-dev
+	sudo apt-get libboost-thread-dev
+	#option 2. if install all boost development packages with
+	#sudo apt-get intall libboost-al-dev
+	#option 3.  build boost yourself
+	#sudo su
+	#./bootrap.sh
+	#./bjam intall
+}
+
+
+
+makeberklydb{
+
+	# Linux (Ubuntu Only) BerkeleyDb Install
 	sudo apt-get install software-properties-common
+	sudo add-apt-repository ppa:bitcoin/bitcoin
 	sudo apt-get update
 	sudo apt-get install libdb4.8-dev libdb4.8++-dev
-	# upnc - Optional (see --with-miniupnpc and --enable-upnp-default):
-	#sudo apt-get install libminiupnpc-dev
-	# zero message queue
-	#sudo apt-get install libzmq3-dev
-	# QT5 - QT Wallet
-	#sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-	# QR
-	#sudo apt-get install libqrencode-dev
 
+}
+
+
+
+makechaincoin{
 	git clone https://github.com/ChainCoin/ChainCoin.git -b Chaincoin_0.16-dev
 	cd ChainCoin
 	./autogen.sh
@@ -91,16 +105,7 @@ prepdependencies() { #TODO: add error detection
 	./configure CPPFLAGS="-fPIC" --disable-tests --without-gui
 	make
 	make install
-
 }
-	
-
-
-#------------------------------------------------
-
-#--------------------------------------------------
-
-
 
 
 success() {
@@ -113,6 +118,9 @@ install() {
 	#createfirewall
 	#createswap
         prepdependencies
+	#makeboost
+	#makeberklydb
+	#makechaincion
 	success
 }
 
